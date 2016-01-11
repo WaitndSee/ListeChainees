6908deeea9fd6cf9f55daaba7f0cc0550f7592b3
Solene Granjeon <solene.granjeon-noriot@insa-lyon.fr>
2016-01-11 16:40:59 +0100

    Vector comments

--- Vector.cpp
+++ Vector.cpp
@@ -1,29 +1,32 @@
-
-
+#include <cassert>
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
+#include <cstdlib>
 #include "Vector.h"
 
+//Default Constructor
 Vector::Vector(){
 	x=0;
 	y=0;
 	norm=0;
 }
 
+//Copy
 Vector::Vector(const Vector& model){
-	x=model.x;
-	y=model.y;
-	norm=pow(pow(x,2)+pow(y,2),0.5);
+	this->x=model.x;
+	this->y=model.y;
+	this->norm=pow(pow(x,2)+pow(y,2),0.5);
 }
 
+//Giving the coordinates
 Vector::Vector(float x,float y){
 	this->x = x;
 	this->y = y;
 	norm=pow(pow(x,2)+pow(y,2),0.5);
 }
 
-
+//Getters
 
 float Vector::get_x(){
 	return x;
@@ -36,13 +39,17 @@ float Vector::get_y(){
 float Vector::get_norm(){
 	return norm;
 }
-	
+
+//Methods to modify the vector
+
+//Addition	
 void Vector::Add(Vector v){
 	x=v.x+x;
 	y=v.y+y;
 	norm=pow(pow(x,2)+pow(y,2),0.5);
 }
 
+//Multiplication by a scalar
 void Vector::Multiply(float k){
 	x=x*k;
 	y=y*k;
