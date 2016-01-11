e958f1af61a10f635a25e38764212708e6bb0f9d
Solene Granjeon <solene.granjeon-noriot@insa-lyon.fr>
2016-01-11 17:00:01 +0100

    Comments

--- Node.cpp
+++ Node.cpp
@@ -1,17 +1,20 @@
 
-#include <stdio.h>
-#include <iostream>
+# include <stdio.h>
 #include <math.h>
+#include <cassert>
+#include <iostream>
+#include <cstdlib>
+
 #include "Node.h"
+#include "Vector.h"
 
 
-
-
+//Contructors
+//Default
 Node::Node(){
 	Vector v2;
 	addr=new Vector(v2);
 	next =nullptr;
-	
 }
 
 /**
@@ -20,29 +23,36 @@ Node::Node(){
  * fermer le new ouvert.
  * */
 
+//Gives a Vector (passage par valeur)
 Node::Node(Vector vec){
 	
 	addr=new Vector(vec);
 	next=nullptr;
 }
-	
+
+//Copy	
 Node::Node(const Node& model){
-	addr=model.addr;
-	next=model.next;
+	this->addr=model.addr;
+	this->next=model.next;
 }
 
+//Setter
 void Node::set_next(Node* n){
 	next=n;
 }
 
+//Getter
+//Next node
 Node* Node::get_next(){
 	return next;
 }
 
-
+//The vector
 Vector* Node::get_address(){
 	return addr;
 }
+
+//Destructor
 Node::~Node(){
 	delete addr;
 }
